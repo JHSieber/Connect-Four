c64ca6f013b3d79a35298b192479a9abc3dc8361
JHSieber <48075418+JHSieber@users.noreply.github.com>
2020-01-17 18:11:57 -0500

    wokring win conditions for horizontal, vertical, and partial solution for diagonals

--- ConnectFour.py
+++ ConnectFour.py
@@ -11,12 +11,14 @@ pygame.display.set_caption("Connect Four")  #sets the name of the window
 def main():
     makeBoard()
     positions = []
-    for i in range(7):
+    for i in range(6):
         positions.append([])
-        for j in range(8):
+        for j in range(7):
             positions[i].append(' ')
     run = True
     color = 'red'
+    won = False
+    #win = 'red'
     while run:
         pygame.time.delay(100)
         
@@ -28,11 +30,10 @@ def main():
             mouseX, mouseY = pygame.mouse.get_pos() #gets the current x and y of the mouse
             color, positions, position = makeTurn(positions, color, mouseX)
             drawMove(color, position)
-        won = False
-        win = 'red'
-        won, win = winner(positions)
+        won, win = winner(positions, color)
         if won:
             print(win, "is the winner")
+            run = False
         pygame.display.update()
         
     # make the initiol board
@@ -49,11 +50,11 @@ def drawMove(color, position):
     drawY = int((row*2+1)*canvasWidth/12)
     pygame.draw.circle(win, co, (drawX, drawY), int(canvasWidth/14), 0)
 
-def makeTurn(positions, color, mouseX):
+def makeTurn(positions, color, mouseX): #had to change some stuff so if something doesnt work it may be here
     if mouseX > 0 and mouseX < canvasWidth/7: #first column
         for i in range(5,-1,-1): #iterate backwards through each row of the columnn from the bottom to the top 
-            if positions[0][i] == ' ': #check if there is already one there
-                positions[0][i] = color #fill spot in array with correct color
+            if positions[i][0] == ' ': #check if there is already one there
+                positions[i][0] = color #fill spot in array with correct color
                 position = 0, i #save the column and the row of the most recent move to draw
                 if color == 'red': #alternate colors
                     color = 'blue'
@@ -64,8 +65,8 @@ def makeTurn(positions, color, mouseX):
                 continue
     if mouseX > canvasWidth/7 and mouseX < 2*canvasWidth/7: #second Column
         for i in range(5,-1,-1):
-            if positions[1][i] == ' ':
-                positions[1][i] = color
+            if positions[i][1] == ' ':
+                positions[i][1] = color
                 position = 1, i 
                 if color == 'red':
                     color = 'blue'
@@ -76,8 +77,8 @@ def makeTurn(positions, color, mouseX):
                 continue
     if mouseX > 2*canvasWidth/7 and mouseX < 3*canvasWidth/7: #third column
         for i in range(5,-1,-1):
-            if positions[2][i] == ' ':
-                positions[2][i] = color
+            if positions[i][2] == ' ':
+                positions[i][2] = color
                 position = 2, i 
                 if color == 'red':
                     color = 'blue'
@@ -88,8 +89,8 @@ def makeTurn(positions, color, mouseX):
                 continue
     if mouseX > 3*canvasWidth/7 and mouseX < 4*canvasWidth/7: #fourth column
         for i in range(5,-1,-1):
-            if positions[3][i] == ' ':
-                positions[3][i] = color
+            if positions[i][3] == ' ':
+                positions[i][3] = color
                 position = 3, i 
                 if color == 'red':
                     color = 'blue'
@@ -100,8 +101,8 @@ def makeTurn(positions, color, mouseX):
                 continue
     if mouseX > 4*canvasWidth/7 and mouseX < 5*canvasWidth/7: #fifth column
         for i in range(5,-1,-1):
-            if positions[4][i] == ' ':
-                positions[4][i] = color
+            if positions[i][4] == ' ':
+                positions[i][4] = color
                 position = 4, i 
                 if color == 'red':
                     color = 'blue'
@@ -112,8 +113,8 @@ def makeTurn(positions, color, mouseX):
                 continue
     if mouseX > 5*canvasWidth/7 and mouseX < 6*canvasWidth/7: #sixth column
         for i in range(5,-1,-1):
-            if positions[5][i] == ' ':
-                positions[5][i] = color
+            if positions[i][5] == ' ':
+                positions[i][5] = color
                 position = 5, i 
                 if color == 'red':
                     color = 'blue'
@@ -124,8 +125,8 @@ def makeTurn(positions, color, mouseX):
                 continue
     if mouseX > 6*canvasWidth/7 and mouseX < canvasWidth: #seventh column
         for i in range(5,-1,-1):
-            if positions[6][i] == ' ':
-                positions[6][i] = color
+            if positions[i][6] == ' ':
+                positions[i][6] = color
                 position = 6, i 
                 if color == 'red':
                     color = 'blue'
@@ -135,17 +136,48 @@ def makeTurn(positions, color, mouseX):
             else:
                 continue
     
-
 def makeBoard():
     run = True
     #TODO
 
+def winner(positions, color):
+    #check for horizontal wins
+    for i in range(6): #iterate through each row
+        total = 1
+        for j in range(1,7): #iterate through all but the first column
+            if positions[i][j] == positions[i][j-1]: #if the next color on the line is the same as the previous then increase total
+                if positions[i][j] != ' ' and positions[i][j-1] != ' ':
+                    total+=1
+                    if total == 4:
+                        return True, color
+            else:
+                total = 1
+    
+    #check for verticle wins
+    for i in range(7): #iterate through each column
+        total = 1
+        for j in range(1,6): #iterate through each row in the column
+            if positions[j][i] == positions[j-1][i]: #check if the color is the same as the one above it
+                if positions[j][i] != ' ' and positions[j-1][i] != ' ':
+                    total+=1
+                    if total == 4:
+                        return True, color
+    
+    #check for diagonal left
+    for i in range(5,2,-1): #checks bottom 3 rows
+        for j in range (3,7): #checks right 4 columns
+            print(checkUpLeft(positions, i, j))
+            if checkUpLeft(positions, i, j) == 4:
+                return True, positions[i][j]
+    return False, ' '
 
-def winner(positions):
-    won = False
-    win = 'red'
-    won, win = horizontalWin(positions)
-    return won, win
+def checkUpLeft(positions, row, col):
+    if col < 0 or row < 0:
+        return 0
+    if positions[row][col] == positions[row-1][col-1] and positions[row][col] != ' ':
+        return 1 + checkUpLeft(positions, col-1, row-1)
+    else:
+        return 0
     
             
 
